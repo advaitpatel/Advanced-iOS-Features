a256a7b9a2ef4b343edfeb09713a48b09fda95a3
Advait Patel <advaitpa93@gmail.com>
2017-02-01 17:27:28 -0600

    Adding Multiple View Controllers
    Jump from one page to another

---
+++ Multiple View Controller/Multiple View Controller/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controller
+//
+//  Created by Patel, Advait Rajeshkumar on 2/1/17.
+//  Copyright © 2017 Patel, Advait. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        print("Second view is ready")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
